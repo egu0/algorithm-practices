a6b12ceb15335d695982fefac502ae47f3888b63
engureguo <8440193+engureguo@user.noreply.gitee.com>
2021-09-10 22:01:34 +0800

    堆和优先级队列

---
+++ src/com/engure/heap/MaxHeap.java
@@ -0,0 +1,136 @@
+package com.engure.heap;
+
+import java.util.Arrays;
+
+/*
+可视化演示：https://visualgo.net/zh/heap
+学习来源：https://www.bilibili.com/video/BV1ti4y1879c
+ */
+
+public class MaxHeap {
+
+    /*
+    堆排序：将所有元素插入一个堆中，不断取出堆顶元素
+     */
+    public static void main(String[] args) {
+        int[] arr = {-1, 8, 2, 100, 30, 13, 66};
+        MaxHeap maxHeap = new MaxHeap(arr.length);
+        for (int e : arr) {
+            maxHeap.add(e);
+        }
+        while (maxHeap.size() != 0) {
+            System.out.println(maxHeap.poll() + " ");
+        }
+
+    }
+
+    int size = 0;
+    int len;
+    int[] arr;//存放元素
+
+    public MaxHeap(int len) {
+        if (len < 1) throw new RuntimeException("illegal length");
+        this.len = len;
+        arr = new int[len];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    /*
+    取堆顶并删除，O(logN)
+     */
+    public int poll() {
+        if (size == 0) throw new RuntimeException("empty heap");
+        int ele = arr[0];
+        arr[0] = arr[size - 1];
+        arr[--size] = 0;
+        heapifyDown();
+        return ele;
+    }
+
+    /*
+    下沉
+     */
+    private void heapifyDown() {
+        int index = 0;//从新堆顶开始
+        int largerChild, newIndex, tmp;
+        while (getLeftChildIndex(index) < size) {//如果有左孩子
+            newIndex = getLeftChildIndex(index);
+            largerChild = getLeftChild(index);
+            //如果有右孩子
+            if (getRightChildIndex(index) < size && getRightChild(index) > largerChild) {
+                largerChild = getRightChild(index);
+                newIndex = getRightChildIndex(index);
+            }
+
+            //有大孩子
+            if (largerChild > arr[index]) {
+                tmp = arr[index];
+                arr[index] = largerChild;
+                arr[newIndex] = tmp;
+                index = newIndex;
+            } else break;//没有大孩子则停止
+
+        }
+    }
+
+    /*
+    堆顶，O(1)
+     */
+    public int peek() {
+        if (size == 0) throw new RuntimeException("empty heap");
+        return arr[0];
+    }
+
+    /*
+    入堆，O(logN)
+     */
+    public void add(int val) {
+        if (size == len) {
+            arr = Arrays.copyOf(arr, len * 2);
+            len *= 2;
+        }
+        arr[size++] = val;
+        heapifyUp(size - 1);
+    }
+
+    /*
+    上浮
+     */
+    private void heapifyUp(int index) {
+        int tmp;
+        while (index != 0 && getParent(index) < arr[index]) {
+            tmp = arr[index];
+            arr[index] = getParent(index);
+            arr[getParentIndex(index)] = tmp;
+            index = getParentIndex(index);
+        }
+    }
+
+    private int getLeftChildIndex(int index) {
+        return index * 2 + 1;
+    }
+
+    private int getRightChildIndex(int index) {
+        return index * 2 + 2;
+    }
+
+    private int getParentIndex(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int getLeftChild(int index) {
+        return arr[getLeftChildIndex(index)];//exp
+    }
+
+    private int getRightChild(int index) {
+        return arr[getRightChildIndex(index)];//exp
+    }
+
+    private int getParent(int index) {
+        return arr[getParentIndex(index)];
+    }
+
+}
