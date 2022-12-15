944507d898902a6cba880c11d3395309cd124785
engureguo <8440193+engureguo@user.noreply.gitee.com>
2022-12-15 20:34:05 +0800

    12.14

--- src/java/im/engure/testcases/T2.java
+++ src/java/im/engure/testcases/T2.java
@@ -6,7 +6,28 @@ import java.util.PriorityQueue;
  * @author engure
  */
 public class T2 {
+
+    protected int a = 2;
+
+    static {
+        System.out.println("静态代码块");
+    }
+
+    {
+        System.out.println("代码块");
+    }
+
+    public T2() {
+        System.out.println("构造器代码块");
+    }
+
     public static void main(String[] args) {
+
+        new T2();
+
+        if (true) {
+            return;
+        }
 
         //[0] - 元素，[1] - 位置
         PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]);
