dda73648a99a77795af6550461de91e71b90d09c
egu0 <57861661+engureguo@users.noreply.github.com>
2024-08-15 12:13:30 +0800

    提交之前的代码

--- src/com/engure/array/RepeatedNTimes961.java
+++ src/com/engure/array/RepeatedNTimes961.java
@@ -1,5 +1,8 @@
 package com.engure.array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RepeatedNTimes961 {
 	/**
 	 * arr_len [4, 10000]
@@ -14,7 +17,15 @@ public class RepeatedNTimes961 {
 	 * @return
 	 */
     public int repeatedNTimes(int[] nums) {
-    	
-    	return 1;
+    	Set<Integer> s = new HashSet<>();
+        for(int i=0; i<nums.length; i++) {
+            if (s.contains(nums[i])) {
+                return nums[i];
+            }
+            else {
+                s.add(nums[i]);
+            }
+        }
+        return -1;
     }
 }
